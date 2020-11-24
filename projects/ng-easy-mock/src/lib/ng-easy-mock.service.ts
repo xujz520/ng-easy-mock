98883fbb8f46cbe7f8742d2459e1a024ea4f3817
徐俊洲 <765550360@qq.com>
2020-11-24 20:46:26 +0800

    123123

--- projects/ng-easy-mock/src/lib/ng-easy-mock.service.ts
+++ projects/ng-easy-mock/src/lib/ng-easy-mock.service.ts
@@ -7,25 +7,60 @@ import { MockConfig, Config } from './interfaces';
 })
 export class NgEasyMockService {
 
-  ruleMap = {};
+  ruleList: { method: string, url: string, data: any }[] = [];
 
   constructor(@Optional() @Inject(Config) public config: MockConfig) {
     this.config = Object.assign({ delay: 300, log: true }, this.config);
 
     Object.values(this.config.data || {}).forEach(obj => {
-      Object.assign(this.ruleMap, obj);
+      Object.entries(obj).forEach(([key, value]) => {
+        if (!key.includes(' ')) {
+          key = `GET ${key}`;
+        }
+        let ruleUrls = key.split(' ');
+        this.ruleList.push({
+          method: ruleUrls[0].toLocaleUpperCase(),
+          url: ruleUrls[1],
+          data: value
+        });
+      });
     });
 
-    // GET 补全
-    Object.entries(this.ruleMap).forEach(([key, value]) => {
-      if(!key.includes(' ')) {
-        delete this.ruleMap[key];
-        this.ruleMap[`GET ${key}`] = value;
-      }
-    });
   }
 
-  getRule(method, url): any {
-    return this.ruleMap[`${method} ${url}`];
+  getRule(method: string, url: string): any {
+    method = method.toLocaleUpperCase();
+    url = this.getPath(url);
+
+    let rule = this.ruleList.filter(item => item.method == method).find(item => {
+      let urlSegmentList = this.getUrlSegment(url);
+      let ruleUrlSegmentList = this.getUrlSegment(item.url);
+      if (urlSegmentList.length == ruleUrlSegmentList.length) {
+        return ruleUrlSegmentList.every((ruleUrlSegment, index) => {
+          if(ruleUrlSegment.startsWith(':')) return true;
+          return ruleUrlSegment == urlSegmentList[index];
+        });
+      } else {
+        return false;
+      }
+    });
+
+    return rule?.data;
+  }
+
+  /**
+   * 移除查询字符串
+   * @param url 
+   */
+  getPath(url: string) {
+    return url.slice(0, url.indexOf('?') || undefined);
+  }
+
+  /**
+   * 获取URL片段
+   * @param url 
+   */
+  getUrlSegment(url: string) {
+    return url.split('/');
   }
 }
