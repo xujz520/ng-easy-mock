33bf212ab6c85eca808ad44c77ac46317077d7b0
xjz <765550360@qq.com>
2020-11-26 18:52:52 +0800

    build

--- dist/ng-easy-mock/lib/ng-easy-mock.module.d.ts
+++ dist/ng-easy-mock/lib/ng-easy-mock.module.d.ts
@@ -1,11 +1,5 @@
 import { ModuleWithProviders } from '@angular/core';
 import { MockConfig } from './interfaces';
-import * as ɵngcc0 from '@angular/core';
-import * as ɵngcc1 from '@angular/common';
 export declare class NgEasyMockModule {
     static forRoot(config: MockConfig): ModuleWithProviders<NgEasyMockModule>;
-    static ɵmod: ɵngcc0.ɵɵNgModuleDefWithMeta<NgEasyMockModule, never, [typeof ɵngcc1.CommonModule], never>;
-    static ɵinj: ɵngcc0.ɵɵInjectorDef<NgEasyMockModule>;
 }
-
-//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibmctZWFzeS1tb2NrLm1vZHVsZS5kLnRzIiwic291cmNlcyI6WyJuZy1lYXN5LW1vY2subW9kdWxlLmQudHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IkFBQUE7QUFDQTs7O0FBQ0E7QUFDQTs7O0FBQ0EiLCJzb3VyY2VzQ29udGVudCI6WyJpbXBvcnQgeyBNb2R1bGVXaXRoUHJvdmlkZXJzIH0gZnJvbSAnQGFuZ3VsYXIvY29yZSc7XHJcbmltcG9ydCB7IE1vY2tDb25maWcgfSBmcm9tICcuL2ludGVyZmFjZXMnO1xyXG5leHBvcnQgZGVjbGFyZSBjbGFzcyBOZ0Vhc3lNb2NrTW9kdWxlIHtcclxuICAgIHN0YXRpYyBmb3JSb290KGNvbmZpZzogTW9ja0NvbmZpZyk6IE1vZHVsZVdpdGhQcm92aWRlcnM8TmdFYXN5TW9ja01vZHVsZT47XHJcbn1cclxuIl19
